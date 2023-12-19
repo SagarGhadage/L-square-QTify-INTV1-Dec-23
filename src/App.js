b1be95073d9ca6d7dac2c54bd3181c0e6f7955a4
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2023-12-19 11:18:38 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Button from './components/Button/Button';
 import HeroSession from './components/HeroSession/HeroSession';
+import Logo from './components/Logo/Logo';
 import Navbar from './components/Navbar/Navbar';
+import Search from './components/Search/Search';
 function App() {
   return (<>
-  <Navbar></Navbar>
-  <HeroSession/>
-    </>
+    <Navbar>
+      <Logo />
+      <Search placeH={"Search a album of your choice"} />
+      <Button>Give feedback</Button>
+    </Navbar>
+    <HeroSession />
+  </>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,7 @@ import Search from '../Search/Search';
 // import { ReactComponent as HeadPhoneIcon } from "../../assets/Logo.svg";
 // import { ReactComponent as QtifyLogo } from "../../assets/Q tify.svg";
 import Logo from "../Logo/Logo";
-export default function Navbar({ comp }) {
-    return <nav className={styles.navbarContainer}>
-        <Logo/>
-        <Search placeH={"Search a album of your choice"} />
-        <Button>Give feedback</Button>
+export default function Navbar({ children }) {
+    return <nav className={styles.navbarContainer}>{children}
     </nav>
 }
